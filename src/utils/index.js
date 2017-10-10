9b7243b71036a729ca8e41e61405dc65614b3583
Floyd Price <f.price@floydprice.com>
2017-10-10 18:21:26 +0100

    isLoggedIn helper middleware

---
+++ src/utils/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function isLoggedIn(req, res, next) {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  return res.status(302).redirect('/auth');
+}
+
+module.exports = {isLoggedIn};
