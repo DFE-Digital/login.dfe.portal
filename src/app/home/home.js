381781f07e9a0664814a1a15fe13b95891f706d8
Floyd Price <f.price@floydprice.com>
2017-10-11 21:51:20 +0100

    put the logged in user on the header (@jamesmking please fix the html)

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -3,7 +3,8 @@ const {getUserDisplayName} = require('../../infrastructure/utils');
 
 const home = (req, res) => {
   res.render('home/views/home', {
-    displayName: req.user ? getUserDisplayName(req.user) : 'Unknown User'
+    displayName: req.user ? getUserDisplayName(req.user) : 'Unknown User',
+    user: req.user
   });
 };
 
