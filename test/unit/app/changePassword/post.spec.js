1b7cc6685851e25034bf79828d7e22fa1b7c456d
Simon Bull <simon.bull.84@googlemail.com>
2017-10-17 15:33:49 +0100

    Add tests for change password post action

---
+++ test/unit/app/changePassword/post.spec.js
@@ -0,0 +1,154 @@
+jest.mock('./../../../../src/infrastructure/config');
+jest.mock('./../../../../src/infrastructure/account');
+
+describe('when processing a users request to change password', () => {
+
+  let validatePassword;
+  let setPassword;
+  let req;
+  let render;
+  let flash;
+  let redirect;
+  let res;
+  let action;
+
+  beforeEach(() => {
+    const config = require('./../../../../src/infrastructure/config');
+    config.mockImplementation(() => {
+      return {};
+    });
+
+    validatePassword = jest.fn().mockReturnValue(true);
+
+    setPassword = jest.fn();
+
+    const Account = require('./../../../../src/infrastructure/account');
+    Account.fromContext = jest.fn().mockImplementation(() => {
+      return {
+        validatePassword,
+        setPassword,
+      };
+    });
+
+    req = {
+      csrfToken: () => {
+        return 'token';
+      },
+      body: {
+        oldPassword: 'password',
+        newPassword: 'new-password',
+        confirmPassword: 'new-password',
+      },
+    };
+
+    render = jest.fn();
+    flash = jest.fn();
+    redirect = jest.fn();
+    res = {
+      render,
+      flash,
+      redirect,
+    };
+
+    action = require('./../../../../src/app/changePassword/post');
+  });
+
+  it('then it should return failed view if old password not entered', async () => {
+    req.body.oldPassword = '';
+
+    await action(req, res);
+
+    expect(render.mock.calls.length).toBe(1);
+    expect(render.mock.calls[0][0]).toBe('changepassword/views/change');
+    expect(render.mock.calls[0][1]).toMatchObject({
+      validationFailed: true,
+      validationMessages: {
+        oldPassword: 'Must enter current password',
+      },
+    });
+  });
+
+  it('then it should return failed view if new password not entered', async () => {
+    req.body.newPassword = '';
+
+    await action(req, res);
+
+    expect(render.mock.calls.length).toBe(1);
+    expect(render.mock.calls[0][0]).toBe('changepassword/views/change');
+    expect(render.mock.calls[0][1]).toMatchObject({
+      validationFailed: true,
+      validationMessages: {
+        newPassword: 'Must enter your new password',
+      },
+    });
+  });
+
+  it('then it should return failed view if confirm password not entered', async () => {
+    req.body.confirmPassword = '';
+
+    await action(req, res);
+
+    expect(render.mock.calls.length).toBe(1);
+    expect(render.mock.calls[0][0]).toBe('changepassword/views/change');
+    expect(render.mock.calls[0][1]).toMatchObject({
+      validationFailed: true,
+      validationMessages: {
+        confirmPassword: 'Must enter confirm new password',
+      },
+    });
+  });
+
+  it('then it should return failed view if confirm password does not match new password', async () => {
+    req.body.newPassword = 'password1';
+    req.body.confirmPassword = 'password2';
+
+    await action(req, res);
+
+    expect(render.mock.calls.length).toBe(1);
+    expect(render.mock.calls[0][0]).toBe('changepassword/views/change');
+    expect(render.mock.calls[0][1]).toMatchObject({
+      validationFailed: true,
+      validationMessages: {
+        confirmPassword: 'Passwords do not match',
+      },
+    });
+  });
+
+  it('then it should return failed view if old password is not valid', async () => {
+    validatePassword.mockReturnValue(false);
+
+    await action(req, res);
+
+    expect(render.mock.calls.length).toBe(1);
+    expect(render.mock.calls[0][0]).toBe('changepassword/views/change');
+    expect(render.mock.calls[0][1]).toMatchObject({
+      validationFailed: true,
+      validationMessages: {
+        oldPassword: 'We don\'t recognise that password. Please check and try again.',
+      },
+    });
+  });
+
+  it('then it should set user password if validation passes', async () => {
+    await action(req, res);
+
+    expect(setPassword.mock.calls.length).toBe(1);
+    expect(setPassword.mock.calls[0][0]).toBe('new-password');
+  });
+
+  it('then it should set flash message if validation passes', async () => {
+    await action(req, res);
+
+    expect(flash.mock.calls.length).toBe(1);
+    expect(flash.mock.calls[0][0]).toBe('info');
+    expect(flash.mock.calls[0][1]).toBe('Your password has been changed');
+  });
+
+  it('then it should redirect to profile if validation passes', async () => {
+    await action(req, res);
+
+    expect(redirect.mock.calls.length).toBe(1);
+    expect(redirect.mock.calls[0][0]).toBe('/profile');
+  });
+
+});
