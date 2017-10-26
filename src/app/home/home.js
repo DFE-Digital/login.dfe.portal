9a30eb832a61ea4a9f3878fe0f479a043c3f8d5a
Simon Bull <simon.bull.84@googlemail.com>
2017-10-26 09:05:50 +0100

    Add static list of available services

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -1,10 +1,14 @@
 'use strict';
 const {getUserDisplayName} = require('../../infrastructure/utils');
+const {getAvailableServicesForUser} = require('../../infrastructure/services');
 
-const home = (req, res) => {
+const home = async (req, res) => {
+  const availableServices = await getAvailableServicesForUser(req.user.sub);
+
   res.render('home/views/home', {
     displayName: req.user ? getUserDisplayName(req.user) : 'Unknown User',
-    user: req.user
+    user: req.user,
+    availableServices,
   });
 };
 
