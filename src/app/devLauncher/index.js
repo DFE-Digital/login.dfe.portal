1d7983a89e155de9f30043790910fc6607b037f9
Floyd Price <f.price@floydprice.com>
2017-10-31 21:47:26 +0000

    new routes section of the dev launcher

--- src/app/devLauncher/index.js
+++ src/app/devLauncher/index.js
@@ -5,15 +5,17 @@ const uuid = require('uuid/v4');
 const {isLoggedIn} = require('../../infrastructure/utils');
 const logger = require('../../infrastructure/logger');
 
+
 const router = express.Router({ mergeParams: true });
 
-const devLauncher = (csrf) => {
+const devLauncher = (csrf, routes) => {
   logger.info('Mounting dev routes');
 
   router.get('/', isLoggedIn, (req, res) => {
     res.render('devLauncher/views/launchpad', {
       uuid: uuid(),
-      user: req.user
+      user: req.user,
+      routes
     });
   });
 
