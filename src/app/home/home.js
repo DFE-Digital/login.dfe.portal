d0e432c8f1ef01e2b406c50d0695ce8c506611e7
James King <hello@jamesmichaelking.com>
2017-11-29 11:09:56 +0000

    Markup for services portal

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -5,16 +5,12 @@ const { groupBy, keys } = require('lodash');
 
 const home = async (req, res) => {
   const myServices = await getServicesForUser(req.user.sub);
-  const myServicesByOrganisations = groupBy(myServices.filter(e => e.status === 1), s => s.organisation.id);
-  const myPendingServicesByOrganisations = groupBy(myServices.filter(e => e.status !== 1), s => s.organisation.id);
+  const myServicesByOrganisations = groupBy(myServices, s => s.organisation.id);
   const myOrganisations = keys(myServicesByOrganisations);
-  const myPendingOrganisations = keys(myPendingServicesByOrganisations);
   res.render('home/views/home', {
     title: 'Access DfE services',
     myServicesByOrganisations,
-    myPendingServicesByOrganisations,
     myOrganisations,
-    myPendingOrganisations,
   });
 };
 
