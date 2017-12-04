68a30232b591b95e4e84440ada81da95300e0087
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-12-04 15:33:00 +0000

    Add request date to returned model

--- src/infrastructure/services/Service.js
+++ src/infrastructure/services/Service.js
@@ -1,11 +1,12 @@
 class Service {
-  constructor({ id, name, description, organisation, role, status }) {
+  constructor({ id, name, description, organisation, role, status, requestDate }) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.organisation = organisation;
     this.role = role;
     this.status = status;
+    this.requestDate = requestDate;
   }
 }
 
